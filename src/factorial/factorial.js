56fcc6cc5c2745a3ca49b47a0a750d7be95b31d4
Sabrina Silva <lima.sbrn@gmail.com>
2022-07-08 20:06:43 -0300

    init commit test

---
+++ src/factorial/factorial.js
@@ -0,0 +1,19 @@
+import { typeErrorMessage , rangeErrorMessage } from "./utils"
+
+const getFactorial = number => {
+    if (typeof number !== 'number'){
+        throw new TypeError(typeErrorMessage)
+    }
+
+    if (number < 0 || number > 20){
+        throw new RangeError(rangeErrorMessage)  
+     }
+
+    if (number === 1 || number === 0) {
+        return 1 
+    }
+
+ return number * getFactorial(number - 1)
+}
+
+export default getFactorial0
---
+++ src/factorial/utils.js
@@ -0,0 +1,2 @@
+export const typeErrorMessage = 'getFactorial should receive a number as argument'
+export const rangeErrorMessage = 'getFactorial should receive a number bigger than zero and smaller than twenty'
